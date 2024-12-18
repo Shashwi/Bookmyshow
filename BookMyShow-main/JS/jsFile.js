6851c723a2a3253e6e87097c6c6a45f8164b6cb2
Sharatha kumara sajjana <143533127+Shashwi@users.noreply.github.com>
2024-12-18 22:34:13 +0530

    Add files via upload

---
+++ BookMyShow-main/JS/jsFile.js
@@ -0,0 +1,11 @@
+
+var a=document.getElementById("imageChange");
+var b=document.getElementById("imageToChange");
+
+a.addEventListener("click", function() {
+  b.setAttribute("src","assets/open2.jpg")
+});
+
+//This script changes the image source when the button is clicked.
+
+
